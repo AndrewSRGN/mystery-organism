4aef182635a1367fedefe4be4a9ef295d70d8e1d
Andrei Siarheyenka <andrew.srgn@gmail.com>
2023-12-14 10:36:38 -0800

    TIK-007/ update func mestRelatedSpecimen &  mestRelatedSpecimenAll (also compare complement dna)

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const createSpecimen = require('./functions/createSpecimen');
+const pAequorFactory = require('./functions/pAequorFactory');
 
-const specimens = createSpecimen(30);
+const specimens = createSpecimen(200);
+
+
+specimens.forEach( specimen =>{
+    console.log(`Specimen #${specimen.specimenNum} DNA = ${specimen.dna}`);
+})
+
 
 const mostRelatedSpecimen = (specimens) => {
     let specimenTarget = {};
     let specimenSource = {};
 
     let similarity = 0;
+    let isComplement = false;
 
     for (let i = 0; i < specimens.length - 1; i++) {
-        for (let j = i + 1; j < specimens.length - 1; j++) {
-            if (specimens[i].compareDNA(specimens[j]) > similarity) {
-                similarity = specimens[i].compareDNA(specimens[j]);
+        for (let j = i + 1; j < specimens.length; j++) {
+            const complementDNA = specimens[i].complementStrand();
+
+            let complementSpecimen = {};
+            complementSpecimen = Object.assign(complementSpecimen, specimens[i]);
+            complementSpecimen.dna = complementDNA;
+
+            if (specimens[i].compareDNA(specimens[j]) > similarity ||
+                complementSpecimen.compareDNA(specimens[j]) > similarity) {
+                isComplement = complementSpecimen.compareDNA(specimens[j]) > similarity;
+                similarity = complementSpecimen.compareDNA(specimens[j]);
                 specimenTarget = specimens[i];
                 specimenSource = specimens[j];
             }
@@ -21,7 +37,8 @@ const mostRelatedSpecimen = (specimens) => {
     return {
         target: specimenTarget,
         source: specimenSource,
-        similarity: similarity
+        similarity: similarity,
+        isComplement: isComplement
     };
 }
 
@@ -30,12 +47,21 @@ const mostRelatedSpecimenAll = (specimens) => {
     const mostRelatedSpecimens = [];
 
     for (let i = 0; i < specimens.length - 1; i++) {
-        for (let j = i + 1; j < specimens.length - 1; j++) {
-            if (specimens[i].compareDNA(specimens[j]) === similarity) {
+        for (let j = i + 1; j < specimens.length; j++) {
+            const complementDNA = specimens[i].complementStrand();
+
+            let complementSpecimen = {};
+            complementSpecimen = Object.assign(complementSpecimen, specimens[i]);
+            complementSpecimen.dna = complementDNA;
+
+            if (specimens[i].compareDNA(specimens[j]) === similarity ||
+                complementSpecimen.compareDNA(specimens[j]) === similarity) {
+                const isComplement = complementSpecimen.compareDNA(specimens[j]) === similarity;
                 const relatedSpecimens = {
                     target: specimens[i],
                     source: specimens[j],
-                    similarity: similarity
+                    similarity: similarity,
+                    isComplement: isComplement
                 }
 
                 mostRelatedSpecimens.push(relatedSpecimens);
@@ -49,5 +75,8 @@ const mostRelatedSpecimenAll = (specimens) => {
 const results = mostRelatedSpecimenAll(specimens);
 console.log(`The most similar pairs of specimens is:`)
 for (let result of results) {
-    console.log(`Specimen #${result.target.specimenNum} and specimen #${result.source.specimenNum} which have ${Math.round(result.similarity * 100 * 100) / 100}% DNA in common.`);
+    console.log(`Specimen #${result.target.specimenNum}${result.isComplement ? ' complement' : ''} and specimen #${result.source.specimenNum} which have ${Math.round(result.similarity * 100 * 100) / 100}% DNA in common.
+Specimen #${result.target.specimenNum} DNA = ${result.target.dna} 
+Specimen #${result.source.specimenNum} DNA = ${result.source.dna}
+`);
 }
