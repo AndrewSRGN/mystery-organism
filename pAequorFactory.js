c4071c0887f86047a89dd9cc91e4e9e068dfe7c2
Andrei Siarheyenka <andrew.srgn@gmail.com>
2023-12-13 23:15:50 -0800

    TIK-002/ add method '.compare' in pAeqor ogject

--- pAequorFactory.js
+++ pAequorFactory.js
@@ -5,6 +5,13 @@ const pAequorFactory = (specimenNum, dna) => {
         _specimenNum: specimenNum,
         _dna: dna,
 
+        get specimenNum () {
+            return this._specimenNum;
+        },
+        get dna () {
+            return this._dna;
+        },
+
         mutate () {
             const randDnaIndex = Math.floor(Math.random() * this._dna.length);
 
@@ -13,6 +20,22 @@ const pAequorFactory = (specimenNum, dna) => {
 
             this._dna[randDnaIndex] = returnRandBase(dnaBases);
         },
+
+        compareDNA (pAequor) {
+            const targetDna = pAequor.dna;
+            const sourceDna = this.dna;
+            let compareBases = 0;
+
+            for (let i = 0; i < targetDna.length; i++) {
+                if (targetDna[i] === sourceDna[i]) {
+                    compareBases++;
+                }
+            }
+
+            console.log(`Specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${compareBases/targetDna.length * 100}% DNA in common.`);
+
+            return compareBases/targetDna.length;
+        }
     }
 }
 
