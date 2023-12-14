5c38ebdc63750f46a7c09674cf2b63977b0dab5e
Andrei Siarheyenka <andrew.srgn@gmail.com>
2023-12-14 02:41:10 -0800

    TIK-006/ add func mostRelatedSpecimenAll

--- main.js
+++ main.js
@@ -25,9 +25,29 @@ const mostRelatedSpecimen = (specimens) => {
     };
 }
 
-const result = mostRelatedSpecimen(specimens);
+const mostRelatedSpecimenAll = (specimens) => {
+    const similarity = mostRelatedSpecimen(specimens).similarity;
+    const mostRelatedSpecimens = [];
 
-console.log(`
-The most similar pair of specimens is:
-Specimen #${result.target.specimenNum} and specimen #${result.source.specimenNum} which have ${Math.round(result.similarity * 100 * 100) / 100}% DNA in common.`
-);
+    for (let i = 0; i < specimens.length - 1; i++) {
+        for (let j = i + 1; j < specimens.length - 1; j++) {
+            if (specimens[i].compareDNA(specimens[j]) === similarity) {
+                const relatedSpecimens = {
+                    target: specimens[i],
+                    source: specimens[j],
+                    similarity: similarity
+                }
+
+                mostRelatedSpecimens.push(relatedSpecimens);
+            }
+        }
+    }
+
+    return mostRelatedSpecimens;
+}
+
+const results = mostRelatedSpecimenAll(specimens);
+console.log(`The most similar pairs of specimens is:`)
+for (let result of results) {
+    console.log(`Specimen #${result.target.specimenNum} and specimen #${result.source.specimenNum} which have ${Math.round(result.similarity * 100 * 100) / 100}% DNA in common.`);
+}
